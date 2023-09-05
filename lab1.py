c987198ecf2d24796a9bda5dfe23baf76aaa4278
Jennifer <je8720su-s@student.lu.se>
2023-09-05 14:33:11 +0200

    almost done with I

--- lab1.py
+++ lab1.py
@@ -5,19 +5,24 @@ with open("../korpus", "r", encoding = "latin-1") as f:
     #print(f.read(5000))
     with open("../I.txt", "a", encoding = "latin-1") as I:
         byteCounter = 0
-        byteStart = 1
+        byteStart = 0
         while True:
             oneChar = f.read(1)
-            byteCounter += 1
-            if(oneChar == " " or oneChar == "\\n" or oneChar == "." or oneChar == "," or oneChar == ","):
+            
+            #if(oneChar == " " or oneChar == "\\" or oneChar == "." or oneChar == "," or oneChar == "-"):
+            if(oneChar.isalpha() == False):
                 word = ""
                 for i in range(byteStart, byteCounter):
                     f.seek(i)
                     word += f.read(1)
+                f.seek(byteCounter + 1)
                 I.write(word + " " + str(byteStart) + "\n")
-                byteStart = byteCounter 
+                byteStart = byteCounter + 1
             elif(oneChar == ""):
                 break
+
+            byteCounter += 1
+
             if(byteCounter == 100):
                 break
         
