6756994595645b01f7f2e4f5ad01ee839d1aeb3c
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-09-05 21:13:57 +0200

    jag har inget liv
    hashfunktion

---
+++ hash.py
@@ -0,0 +1,20 @@
+def hashHelper(number):
+    if(number == 32):
+        return 113
+    elif(number < 123 ):
+        return number - 97
+    elif(number < 230):
+        return number - 201
+    else:
+        return number - 217
+
+def hash(word):
+    first = hashHelper(ord(word[0].lower()))
+    second = hashHelper(ord(word[1].lower()))
+    third = hashHelper(ord(word[2].lower()))
+
+    return((first*900)+(second*30)+third)
+
+#bara för att testa
+print("Please enter a word:")
+userInput = input() + "   "
--- lab1.py
+++ lab1.py
@@ -1,15 +1,9 @@
-
-
-
 with open("../korpus", "r", encoding = "latin-1") as f:
-    #print(f.read(5000))
     with open("../I.txt", "a", encoding = "latin-1") as I:
         byteCounter = 0
         byteStart = 0
         while True:
             oneChar = f.read(1)
-            
-            #if(oneChar == " " or oneChar == "\\" or oneChar == "." or oneChar == "," or oneChar == "-"):
             if(oneChar.isalpha() == False):
                 word = ""
                 for i in range(byteStart, byteCounter):
@@ -26,17 +20,8 @@ with open("../korpus", "r", encoding = "latin-1") as f:
                     f.seek(byteCounter + 1)
             elif(oneChar == ""):
                 break
-
             byteCounter += 1
-
             if(byteCounter == 100):
                 break
-        
     
-    
-    '''with open("../I.txt", "a+", encoding = "latin-1") as I:
-        I.write("hej2")
-        print(I.read())  '''      
-    
-
 
