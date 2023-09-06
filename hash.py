a32af0b2f5fc0c10a75dcb0152dd32bcba91f800
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-09-06 13:28:21 +0200

    hej

--- hash.py
+++ hash.py
@@ -1,6 +1,6 @@
 def hashHelper(number):
     #tänker att vi kanske kan lägga till "   " efter alla ord som inputas 
-    #klarar då av ord som t.ex i, ö, är. 32=q minst använda bokstaven i
+    #klarar då av ord som t.ex i, ö, är
     if(number == 32):
         return 29
     elif(number < 123):
@@ -18,7 +18,18 @@ def hash(word):
 
     return((first*900)+(second*30)+third)
 
-#bara för att testa
+'''
 print("Please enter a word:")
 userInput = input()
-print(hash(userInput))
+print(hash(userInput)) '''
+
+AIndex = [-1]*27000
+print(AIndex)
+
+with open("../rawindex.txt", "r", encoding = "latin-1") as f:
+    for i in range (30):
+        for j in range (30):
+            for k in range (30):
+                f.read(1)
+                AIndex[(i*900)+(j*30)+k] = f.readline
+        
