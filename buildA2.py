adee6ddf8468f3e2b734dbdc1914730790d09f3f
Jennifer <je8720su-s@student.lu.se>
2023-09-07 10:27:49 +0200

    done with building A

---
+++ buildA2.py
@@ -0,0 +1,56 @@
+
+def hashHelper(number):
+    if(number == 32):
+        return 0
+    elif(number < 123):
+        return number - 96
+    elif(number < 230):
+        return number - 201
+    else:
+        return number - 217
+
+def hash(word):
+    word += '   '
+    first = hashHelper(ord(word[0].lower()))
+    second = hashHelper(ord(word[1].lower()))
+    third = hashHelper(ord(word[2].lower()))
+
+    return((first*900)+(second*30)+third)
+
+aIndex = [-1]*27000
+
+def makeWord(line):
+    word = ""
+    i = 0
+    if(line == ""):
+        return ""
+    while(line[i].isalpha() and i < 3):
+        word += line[i]
+        i += 1
+    if(len(word) == 1):
+        word = word + "  "
+    elif(len(word) == 2):
+        word = word +  " "
+    return word
+
+with open("../rawindex.txt", "r", encoding = "latin-1") as f:
+    sameword = False
+    byteStart = 0
+    line = f.readline()
+    
+    while(True):
+        word = makeWord(line)
+        if(word == ""):
+            break
+        if(sameword == False):
+            indexPos = hash(word)
+            if(aIndex[indexPos] == -1):
+                aIndex[indexPos] = byteStart
+        byteStart += len(line) 
+        line = f.readline()
+        newWord = makeWord(line)
+        if(word == newWord):
+            sameWord = True
+        else:
+            word = newWord
+            sameWord = False
