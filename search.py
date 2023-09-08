64a8f0828d40122f3e3b5135b78968e262aa3791
Jennifer <je8720su-s@student.lu.se>
2023-09-08 16:47:00 +0200

    maybe work

--- search.py
+++ search.py
@@ -1,5 +1,36 @@
+def hashHelper(number):
+    # a-z becomes 1-26
+    if(number > 96 and number < 123):
+        return number - 96
+    # å and ä becomes 27 resp. 28
+    elif(number == 228 or number == 229):
+        return number - 201
+    # ö becomes 29
+    elif(number == 246):
+        return number - 217
+    # space and all other characters become 0
+    else:
+        return 0
+
+def hash(word):
+    word += '   '
+    first = hashHelper(ord(word[0].lower()))
+    second = hashHelper(ord(word[1].lower()))
+    third = hashHelper(ord(word[2].lower()))
+
+    return((first*900)+(second*30)+third)
+
+aIndex = []
+with open("A.txt", "r", encoding = "latin-1") as A:
+    while (True):
+        nextVar = A.readline()
+        if(nextVar == ""):
+            break
+        aIndex.append(nextVar)
+
 def searchAlg():
     print("Write your search word: ")
+    print(aIndex[900])
     userInput = input().lower()
     posA = hash(userInput)
     posI = aIndex[posA]
@@ -9,23 +40,43 @@ def searchAlg():
     posINext = aIndex[posA + i]
 
     with open("../rawindex.txt", "r", encoding = "latin-1") as I:
-        lower = posI
-        higher = posINext
-        while (higher - lower > 1000):
+        lower = int(posI)
+        higher = int(posINext)
+        '''while (higher - lower > 1000):
             mid = (lower + higher) / 2
             I.seek(mid)
+            I.readline()
             lineList = I.readline().split()
-            if(lineList[0] <= userInput):
+            if(lineList[0] < userInput):
                 lower = mid
-            else:
-                higher = mid
-        I.seek(lower)
-        while(True):
-            lineList = I.readline().split()
-            if(lineList[0] == userInput):
-                #with open("../korpus", "r", encoding = "latin-1") as L:
-                return lineList[1]
             if(lineList[0] > userInput):
-                return "Not found"
+                higher = mid
+            else:
+                lower'''
+        while (True):
+            mid = (lower + higher) / 2
+            I.seek(mid)
+            I.readline
+            lineList = I.readline().split()
+            if(lineList[0] < userInput):
+                lower = mid - 1
+            elif(lineList[0] > userInput):
+                higher = mid + 1
+            else:
+                higher -= 1
+                if(higher == lower):
+                    break
+                
+        I.seek(lower)
+        lineList = []
+        while(True):
+            lineWord = I.readline().split()
+            if(lineWord[0] == userInput):
+                #with open("../korpus", "r", encoding = "latin-1") as L:
+                #return lineList[1]
+                lineList.append(lineWord)
+            if(lineWord[0] > userInput):
+                #return "Not found"
+                return lineList
 
 print(searchAlg())
