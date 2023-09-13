01aaa03b733fd34a9b3cf3048c43d91337aa88bb
RasmussBrodin <rasmuss.brodin@gmail.com>
2023-09-13 16:24:36 +0200

    added comments

--- konkordans.py
+++ konkordans.py
@@ -2,8 +2,7 @@ from hash import hash
 import math
 import sys
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
-
+# Read from A.txt and save to aIndex
 aIndex = []
 with open("A.txt", "r", encoding = "latin-1") as A:
     while (True):
@@ -12,12 +11,14 @@ with open("A.txt", "r", encoding = "latin-1") as A:
             break
         aIndex.append(nextVar)
 
+# Find the number of occurrences. To improve search speed we only read the number 
+# of occurrences to count them, but dont save them.
 def findAmount(lower, userInput):
     count = 0
     with open("../rawindex.txt", "r", encoding = "latin-1") as I:
         I.seek(lower)
         lineWord = I.readline().split()
-        if(lineWord[0] < userInput):
+        if(lineWord[0] < userInput):                # Handles the case where lower is on the line before the searced word
             lineWord = I.readline().split()
         while(lineWord[0] == userInput):
             lineWord = I.readline().split()
@@ -26,6 +27,9 @@ def findAmount(lower, userInput):
                 break  
     return count
 
+# Binary search to find the first appearance in rawindex of the input word or the index before.
+# It may return the index before because lower is not guaranteed to be a byte poistion 
+# at the start of a new line in rawindex.
 def searchAlg(userInput):
     posA = hash(userInput)
     posI = aIndex[posA]
@@ -52,8 +56,8 @@ def searchAlg(userInput):
                 higher = mid
     return lower
 
-
-def findOccurrences(lower, userInput):
+# Finds the first 25 occurrences of the word in rawindex and returns them in a list
+def find25Occurrences(lower, userInput):              # lower is the first occurrance of the searched word
     with open("../rawindex.txt", "r", encoding = "latin-1") as I:
         count = 0
         I.seek(lower)      
@@ -67,8 +71,8 @@ def findOccurrences(lower, userInput):
             if(lineWord[0] == userInput):
                 lineList.append(lineWord)
                 count += 1
-            else: 
-                if(isFirstRun == True):
+            else:
+                if(isFirstRun == True):             # Handles the case where lower is on the line before the searched word
                     isFirstRun = False
                     continue
                 else:
@@ -76,6 +80,7 @@ def findOccurrences(lower, userInput):
             isFirstRun = False
         return lineList
 
+# Read 30 chars before and after the searched word for every occurrnace returns that as a list
 def addChars(lineList):
     if(len(lineList) == 0):
         return -1
@@ -104,6 +109,8 @@ def addChars(lineList):
                 allOccurrences.append(answerLine)
             return allOccurrences
 
+# read from args and keyboard input. Checks if any extra args exists and then that it is valid (Swedish Alphabet)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
 if(len(sys.argv) > 1):
     userInput = sys.argv[1].lower()
 else: 
@@ -121,10 +128,13 @@ while(validWord == False):
         print("Your word is not valid, try again!")
         userInput = input().lower()  
 
+# Run the functions
 lower = searchAlg(userInput)
 amount = findAmount(lower, userInput)
-list25 = findOccurrences(lower, userInput)
+list25 = find25Occurrences(lower, userInput)
 answer = addChars(list25)
+
+# Check if the word was found, if there are more than 25 occurrances, they can be printed but this may take more than 1 sec
 if(answer == -1):
     print("Not found")
 else:
@@ -140,8 +150,8 @@ else:
         if(userI == "yes"):
             with open("../rawindex.txt", "r", encoding = "latin-1") as I:
                 I.seek(lower)     
-                I.readline()
                 lineList = []
+                isFirstRun = True
                 while(True):
                     lineWord = I.readline()
                     if(lineWord == "\n"):   
@@ -149,8 +159,13 @@ else:
                     lineWord = lineWord.split()
                     if(lineWord[0] == userInput):
                         lineList.append(lineWord)
-                    else: 
-                        break
+                    else:
+                        if(isFirstRun == True):             # Handles the case where lower is on the line before the searched word
+                            isFirstRun = False
+                            continue
+                        else:
+                            break
+                    isFirstRun = False
                 answer = addChars(lineList)
                 for i in range(25, len(lineList)):
                     print(answer[i])
