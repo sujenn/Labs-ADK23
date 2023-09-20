67491ff2dcb52ba93f523de7684bcf48e6a80be5
Rasmuss Per Brodin <rpbrodin@kth.se>
2023-09-20 18:17:38 +0200

    changed in closestwords on ubuntu

--- Lab2/ClosestWords.java
+++ Lab2/ClosestWords.java
@@ -1,4 +1,3 @@
-package Lab2;
 /* Labb 2 i DD2350 Algoritmer, datastrukturer och komplexitet    */
 /* Se labbinstruktionerna i kursrummet i Canvas                  */
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
@@ -10,8 +9,7 @@ public class ClosestWords {
 
   int closestDistance = -1;
 
-  static int partDist(String w1, String w2, int w1len, int w2len) {
-    System.out.println(w1len + " " + w2len);
+  int partDist2(String w1, String w2, int w1len, int w2len) {
     if (w1len == 0)
       return w2len;
     if (w2len == 0)
@@ -25,6 +23,43 @@ public class ClosestWords {
     if (deleteLetter < res)
       res = deleteLetter;
     return res;
+  }
+
+  int partDist(String w1, String w2, int w1len, int w2len) {
+    if (w1len == 0) {
+        return w2len;
+    }
+    if (w2len == 0) {
+        return w1len;
+    }
+
+    int[] prevRow = new int[w2len + 1];
+    int[] currRow = new int[w2len + 1];
+
+    for (int j = 0; j < w2len + 1; j++) {
+        prevRow[j] = j;
+    }
+
+    for (int i = 1; i < w1len + 1; i++) {
+        currRow[0] = i;
+        for (int j = 1; j < w2len + 1; j++) {
+            if (w1.charAt(i - 1) != w2.charAt(j - 1)) {
+                int remove = prevRow[j] + 1;
+                int swap = currRow[j - 1] + 1;
+                int add = prevRow[j - 1] + 1;
+                currRow[j] = Math.min(remove, Math.min(swap, add));
+            }
+            else {
+                currRow[j] = prevRow[j - 1];
+            }
+        }
+        // Deep copy of currRow to prevRow
+        for (int k = 0; k < w2len + 1; k++) {
+            prevRow[k] = currRow[k];
+        }
+    }
+ 
+        return prevRow[w2len];
   }
 
   int distance(String w1, String w2) {
@@ -51,9 +86,5 @@ public class ClosestWords {
 
   List<String> getClosestWords() {
     return closestWords;
-  }
-
-  public static void main(String[] args) {
-    System.out.print(partDist("la", "b", 2, 1));
   }
 }
