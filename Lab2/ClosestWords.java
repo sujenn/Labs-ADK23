6e13ab950a586076138c3509e61f7a6b12829ba3
Jennifer <je8720su-s@student.lu.se>
2023-09-25 10:05:20 +0200

    done

--- Lab2/ClosestWords.java
+++ Lab2/ClosestWords.java
@@ -7,24 +7,7 @@ import java.util.List;
 
 public class ClosestWords {
   LinkedList<String> closestWords = null;
-
   int closestDistance = -1;
-
-  /*int partDist2(String w1, String w2, int w1len, int w2len) {
-    if (w1len == 0)
-      return w2len;
-    if (w2len == 0)
-      return w1len;
-    int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
-	(w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
-    int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
-    if (addLetter < res)
-      res = addLetter;
-    int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
-    if (deleteLetter < res)
-      res = deleteLetter;
-    return res;
-  }*/
   int[][] editMatrix = new int[200][200];
   String oldWord = "";
 
@@ -39,9 +22,10 @@ public class ClosestWords {
     int changeFromRow = 0;
     int compare = 0;
     while (compare < Math.min(w2len, oldWord.length())) {
-      if (w2.charAt(compare) == oldWord.charAt(compare)) {
-        changeFromRow++;
+      if (w2.charAt(compare) != oldWord.charAt(compare)) {
+        break;
       }
+      changeFromRow++;
       compare++;
     }
 
@@ -52,7 +36,7 @@ public class ClosestWords {
       editMatrix[0][col] = col;
     }
 
-    for (int row = changeFromRow + 1; row < w2len + 1; row++) { //rätt?
+    for (int row = changeFromRow + 1; row < w2len + 1; row++) {
       for (int col = 1; col < w1len + 1; col++) {
         if (w2.charAt(row - 1) != w1.charAt(col - 1)){
           int remove = editMatrix[row - 1][col] + 1;
@@ -65,14 +49,8 @@ public class ClosestWords {
       }     
     }
     oldWord = w2;
-    /*for(int row = 0; row < w2len + 1; row++){
-      for(int col = 0; col < w1len +1; col++){
-        System.out.print(editMatrix[row][col] + " ");
-      }
-      System.out.println();
-    }*/
 
-    return editMatrix[w2len][w1len];
+    return editMatrix[w2len][w1len]; 
   }
 
   int distance(String w1, String w2) {
