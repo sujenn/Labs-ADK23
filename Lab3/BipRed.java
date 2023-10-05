37b44afebe35f976034df073e0e51648dd6e9ead
Jennifer <je8720su-s@student.lu.se>
2023-10-05 18:19:20 +0200

    klar med punkt ett och två

--- Lab3/BipRed.java
+++ Lab3/BipRed.java
@@ -1,5 +1,4 @@
 package Lab3;
-
 /**
  * Exempel på in- och utdatahantering för maxflödeslabben i kursen
  * ADK.
@@ -13,56 +12,64 @@ package Lab3;
 public class BipRed {
     Kattio io;
     
-    void readBipartiteGraph() {
-	// Läs antal hörn och kanter
-	int x = io.getInt();
-	int y = io.getInt();
-	int e = io.getInt();
-	
-	// Läs in kanterna
-	for (int i = 0; i < e; ++i) {
-	    int a = io.getInt();
-	    int b = io.getInt();
-	}
+    int[][] readBipartiteGraph() {
+		// Läs antal hörn och kanter
+		
+		int x = io.getInt();
+		int y = io.getInt();
+		int e = io.getInt();
+		
+		// Läs in kanterna
+		int[][] all = new int[e + 2][2];
+		all[0][0] = x;
+		all[0][1] = y;
+		all[1][0] = e;
+		for (int i = 0; i < e; ++i) {
+			int a = io.getInt();
+			int b = io.getInt();
+			all[i + 2][0] = a;
+			all[i + 2][1] = b;
+		}
+		return all;
     }
     
     
-    void writeFlowGraph() {
-	int v = 23, e = 0, s = 1, t = 2;
-	
-	// Skriv ut antal hörn och kanter samt källa och sänka
-	io.println(v);
-	io.println(s + " " + t);
-	io.println(e);
-	for (int i = 0; i < e; ++i) {
-	    int a = 1, b = 2, c = 17;
-	    // Kant från a till b med kapacitet c
-	    io.println(a + " " + b + " " + c);
-	}
-	// Var noggrann med att flusha utdata när flödesgrafen skrivits ut!
-	io.flush();
-	
-	// Debugutskrift
-	System.err.println("Skickade iväg flödesgrafen");
+    void writeFlowGraph(int[][] elements) {
+		int v = elements[0][0] + elements[0][1], e = elements[1][0], s = 0, t = v + 1;
+		
+		// Skriv ut antal hörn och kanter samt källa och sänka
+		io.println(v);
+		io.println(s + " " + t);
+		io.println(e);
+		for (int i = 0; i < e; ++i) {
+			int a = elements[i + 2][0], b = elements[i + 2][1], c = 1;
+			// Kant från a till b med kapacitet c
+			io.println(a + " " + b + " " + c);
+		}
+		// Var noggrann med att flusha utdata när flödesgrafen skrivits ut!
+		io.flush();
+		
+		// Debugutskrift
+		System.err.println("Skickade iväg flödesgrafen");
     }
     
     
     void readMaxFlowSolution() {
-	// Läs in antal hörn, kanter, källa, sänka, och totalt flöde
-	// (Antal hörn, källa och sänka borde vara samma som vi i grafen vi
-	// skickade iväg)
-	int v = io.getInt();
-	int s = io.getInt();
-	int t = io.getInt();
-	int totflow = io.getInt();
-	int e = io.getInt();
-	
-	for (int i = 0; i < e; ++i) {
-	    // Flöde f från a till b
-	    int a = io.getInt();
-	    int b = io.getInt();
-	    int f = io.getInt();
-	}
+		// Läs in antal hörn, kanter, källa, sänka, och totalt flöde
+		// (Antal hörn, källa och sänka borde vara samma som vi i grafen vi
+		// skickade iväg)
+		int v = io.getInt();
+		int s = io.getInt();
+		int t = io.getInt();
+		int totflow = io.getInt();
+		int e = io.getInt();
+		
+		for (int i = 0; i < e; ++i) {
+			// Flöde f från a till b
+			int a = io.getInt();
+			int b = io.getInt();
+			int f = io.getInt();
+		}
     }
     
     
@@ -82,25 +89,26 @@ public class BipRed {
     }
     
     BipRed() {
-	io = new Kattio(System.in, System.out);
-	
-	readBipartiteGraph();
-	
-	writeFlowGraph();
-	
-	readMaxFlowSolution();
-	
-	writeBipMatchSolution();
+		io = new Kattio(System.in, System.out);
+		
+		int[][] elements = readBipartiteGraph();
+		
+		writeFlowGraph(elements);
+		
+		readMaxFlowSolution();
+		
+		writeBipMatchSolution();
 
-	// debugutskrift
-	System.err.println("Bipred avslutar\n");
+		// debugutskrift
+		System.err.println("Bipred avslutar\n");
 
-	// Kom ihåg att stänga ner Kattio-klassen
-	io.close();
+		// Kom ihåg att stänga ner Kattio-klassen
+		io.close();
     }
     
     public static void main(String args[]) {
-	new BipRed();
+		new BipRed();
+	
     }
 }
 
