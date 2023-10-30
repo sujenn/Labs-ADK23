4ee47c051955a69ce5f1dafce3f45c960bce5885
Rasmuss Per Brodin <rpbrodin@kth.se>
2023-10-30 14:11:34 +0100

    pushing agiain

--- Lab3/BipRed.java
+++ Lab3/BipRed.java
@@ -35,7 +35,7 @@ public class BipRed {
     
     
     void writeFlowGraph(int[][] elements) {
-		int v = elements[0][0] + elements[0][1], e = elements[1][0], s = 0, t = v + 1;
+		int v = elements[0][0] + elements[0][1], e = elements[1][0], s = v + 1, t = v + 2;
 		
 		// Skriv ut antal hörn och kanter samt källa och sänka
 		io.println(v);
@@ -54,37 +54,51 @@ public class BipRed {
     }
     
     
-    void readMaxFlowSolution() {
+    int[][] readMaxFlowSolution() {
 		// Läs in antal hörn, kanter, källa, sänka, och totalt flöde
 		// (Antal hörn, källa och sänka borde vara samma som vi i grafen vi
 		// skickade iväg)
+		
 		int v = io.getInt();
 		int s = io.getInt();
 		int t = io.getInt();
 		int totflow = io.getInt();
 		int e = io.getInt();
-		
+
+		int[][] flowSolution = new int[e + 3][3];
+		flowSolution[0][0] = v;
+		flowSolution[1][0] = s;
+		flowSolution[1][1] = t;
+		flowSolution[1][2] = totflow;
+		flowSolution[2][0] = e;
+
+
+
 		for (int i = 0; i < e; ++i) {
 			// Flöde f från a till b
 			int a = io.getInt();
 			int b = io.getInt();
 			int f = io.getInt();
+			flowSolution[i + 3][0] = a;
+			flowSolution[i + 3][1] = b;
+			flowSolution[i + 3][2] = f;
 		}
+		return flowSolution;
     }
     
     
-    void writeBipMatchSolution() {
-	int x = 17, y = 4711, maxMatch = 0;
-	
-	// Skriv ut antal hörn och storleken på matchningen
-	io.println(x + " " + y);
-	io.println(maxMatch);
-	
-	for (int i = 0; i < maxMatch; ++i) {
-	    int a = 5, b = 2323;
-	    // Kant mellan a och b ingår i vår matchningslösning
-	    io.println(a + " " + b);
-	}
+    void writeBipMatchSolution(int[][] flowSolution) {
+		int x = flowSolution[][], y = io.getInt(), maxMatch = flowSolution[1][2];
+		
+		// Skriv ut antal hörn och storleken på matchningen
+		io.println(x + " " + y);
+		io.println(maxMatch);
+		
+		for (int i = 0; i < maxMatch; ++i) {
+			int a = io.getInt(), b = io.getInt();
+			// Kant mellan a och b ingår i vår matchningslösning
+			io.println(a + " " + b);
+		}
 	
     }
     
@@ -95,9 +109,9 @@ public class BipRed {
 		
 		writeFlowGraph(elements);
 		
-		readMaxFlowSolution();
+		int[][] flowSolution = readMaxFlowSolution();
 		
-		writeBipMatchSolution();
+		writeBipMatchSolution(flowSolution);
 
 		// debugutskrift
 		System.err.println("Bipred avslutar\n");
