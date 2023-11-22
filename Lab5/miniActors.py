e93d835852003eba39fb9a882fb0b80c38e09cc0
Jennifer <je8720su-s@student.lu.se>
2023-11-22 10:54:30 +0100

    changes

--- Lab5/miniActors.py
+++ Lab5/miniActors.py
@@ -5,6 +5,9 @@ m = int(input())
 roleList = []
 edgesList = []
 checkEdges = [-1] * V
+actorsRoles = [-1] * 3  # Kanske måste ändra
+invalidNodes = []
+
 
 # Read what actors can be which roles
 for _ in range(V):
@@ -22,6 +25,38 @@ for i in range(E):
     checkEdges[edgesList[i][0] - 1] = edgesList[i][1]
     checkEdges[edgesList[i][1] - 1] = edgesList[i][0]
 
+# Find which roles the divas can have
+diva1 = True
 for i in range(len(checkEdges)):
     if len(checkEdges[i]) < V - 1:
-        break
+        if diva1:
+            actorsRoles[0] = [1, i + 1]
+            diva1 = False
+        else: 
+            actorsRoles[1] = [2, i + 1]
+            break
+
+diva1 = 1
+diva2 = 0
+for i in range(2):  # NOT WORKING FOR 2 DIVAS
+    potenialNodes = []
+    for node in edgesList:
+        if node[0] == actorsRoles[0][1] or node[1] == actorsRoles[0][1]:
+            invalidNodes.append(node[0])
+            invalidNodes.append(node[1])
+        if node[0] == actorsRoles[1][1] or node[1] == actorsRoles[1][1]:
+            invalidNodes.append(node[0])
+            invalidNodes.append(node[1])
+        if (node[0] != actorsRoles[0][1] and node[0] != actorsRoles[1][1]) or (node[1] != actorsRoles[0][1] and node[1] != actorsRoles[1][1]):
+            potenialNodes.append([node[0], node[1]])
+
+    for node in potenialNodes:
+        if not(node[0] in invalidNodes):
+            if node[0] in roleList[0]:
+                actorsRoles[0].append(node[0])
+                invalidNodes.append(node[1])
+        if not(node[1] in invalidNodes):
+            if node[1] in roleList[0]:
+                actorsRoles[0].append(node[1])
+                invalidNodes.append(node[0])
+                
